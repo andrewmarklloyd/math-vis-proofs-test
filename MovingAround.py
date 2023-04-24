17387022e73404da9aee8956dd2ab6ed937cd671
Andrew Lloyd <andrew85.lloyd@gmail.com>
2023-04-23 21:28:15 -0700

    Triggering an update to render

--- MovingAround.py
+++ MovingAround.py
@@ -7,3 +7,5 @@ class MovingAround(Scene):
         self.play(square.animate.set_fill(ORANGE))
         self.play(square.animate.scale(0.3))
         self.play(square.animate.rotate(0.4))
+
+# test update
